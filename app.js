b36a7deef3d6b9145bc00ccd4d37798ef706a579
Beatrice (Con) Hahn <hahnb@usc.edu>
2022-05-22 20:15:10 -0700

    Fix displayTime function for minute and second countdown

--- app.js
+++ app.js
@@ -1,32 +1,21 @@
-// function countdownTimerBegin(){
-//     const duration = 2;
+let totalSeconds = 120;
 
-//     //convert total duration into minutes and seconds
-//     let totalSeconds = duration * 60;
-//     let remainder = totalSeconds%60;
-//     let seconds = remainder;
-//     let minutes = (totalSeconds-remainder)/60;
-//     displayMinutes = minutes < 10 ? "0" + minutes : minutes;
-//     displaySeconds = seconds < 10 ? "0" + seconds : seconds;
-
-//     //output the result into the element with id="timer-display"
-//     document.getElementById("timer-display").innerHTML =  displayMinutes + ":" + displaySeconds;
-
-//     //decrement time
-//     totalSeconds -=1;
-
-// }
-
-// function that displays the current time
-function displayTime(totalSeconds){
-    let remainder = totalSeconds%60;
-    let seconds = remainder;
-    let minutes = (totalSeconds-remainder)/60;
-
-    displayMinutes = minutes < 10 ? "0" + minutes : minutes;
-    displaySeconds = seconds < 10 ? "0" + seconds : seconds;
-
-    //output the result into the element with id="timer-display"
-    let x = document.getElementById("timer-display").innerHTML =  displayMinutes + ":" + displaySeconds;
+function displayTime(){
+    const countdown = setInterval(function(){
+        if (totalSeconds == 0){
+            //if timer reaches 0, call clearInterval function
+            clearInterval(countdown);
+        }
+        let remainder = totalSeconds%60;
+        let seconds = remainder;
+        let minutes = (totalSeconds-remainder)/60;
+    
+        displayMinutes = minutes < 10 ? "0" + minutes : minutes;
+        displaySeconds = seconds < 10 ? "0" + seconds : seconds;
+    
+        //output the result into the element with id="timer-display"
+        let x = document.getElementById("timer-display").innerHTML =  displayMinutes + ":" + displaySeconds;
+        //update total seconds remaining by 1
+        totalSeconds -=1;
+    }, 1000);
 }
-
